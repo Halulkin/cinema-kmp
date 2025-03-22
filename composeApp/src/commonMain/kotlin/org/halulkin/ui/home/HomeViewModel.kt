dc39e524d75277bd3e1ee93b3dadbae9f40249bd
halulkin <halulkin@gmail.com>
2025-03-22 10:56:03 +0100

    feat: implement GetMoviesUseCase and integrate with HomeViewModel

--- composeApp/src/commonMain/kotlin/org/halulkin/ui/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/halulkin/ui/home/HomeViewModel.kt
@@ -2,14 +2,19 @@ package org.halulkin.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.halulkin.domain.model.MovieType
+import org.halulkin.domain.usecase.GetMoviesUseCase
 
-class HomeViewModel() : ViewModel() {
+class HomeViewModel(
+    private val getMoviesUseCase: GetMoviesUseCase,
+) : ViewModel() {
 
     private val _stateFlow: MutableStateFlow<HomeState> = MutableStateFlow(HomeState())
     val stateFlow: StateFlow<HomeState> = _stateFlow.asStateFlow()
@@ -35,6 +40,18 @@ class HomeViewModel() : ViewModel() {
     private fun loadMovies(
         movieType: MovieType,
     ) = viewModelScope.launch {
+        _stateFlow.update { it.copy(isLoading = true) }
 
+        getMoviesUseCase(movieType)
+            .onSuccess { movies ->
+                _stateFlow.update {
+                    it.copy(
+                        isLoading = false,
+                        movies = it.movies + (movieType to movies.cachedIn(viewModelScope)),
+                    )
+                }
+            }.onFailure { error ->
+                _stateFlow.update { it.copy(isLoading = false, error = error.message) }
+            }
     }
 }
