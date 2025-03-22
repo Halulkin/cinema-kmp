ef48d27f9eb7debb3309f05e05b3908c78a14b5a
halulkin <halulkin@gmail.com>
2025-03-22 10:54:39 +0100

    feat: add MovieDTO and MoviesResponse data classes for API integration

---
+++ composeApp/src/commonMain/kotlin/org/halulkin/data/api/model/MoviesResponse.kt
@@ -0,0 +1,12 @@
+package org.halulkin.data.api.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MoviesResponse(
+    @SerialName(value = "page") val page: Int,
+    @SerialName(value = "results") val movies: List<MovieDTO>,
+    @SerialName(value = "total_pages") val totalPages: Int,
+    @SerialName(value = "total_results") val totalResults: Int,
+)
