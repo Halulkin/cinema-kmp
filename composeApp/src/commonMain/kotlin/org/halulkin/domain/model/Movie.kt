5048927ba73a807f244695030798476b9a2b0a2c
halulkin <halulkin@gmail.com>
2025-03-22 13:05:43 +0100

    fix: change image property in Movie data class from nullable to non-nullable

--- composeApp/src/commonMain/kotlin/org/halulkin/domain/model/Movie.kt
+++ composeApp/src/commonMain/kotlin/org/halulkin/domain/model/Movie.kt
@@ -3,7 +3,7 @@ package org.halulkin.domain.model
 data class Movie(
     val id: Int,
     val name: String,
-    val image: String?,
+    val image: String,
     val overview: String,
     val voteAverage: Double,
     val voteCount: Int,
