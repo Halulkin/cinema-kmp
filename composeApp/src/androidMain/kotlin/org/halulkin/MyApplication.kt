3fefeab0f8e619eccdf3c86416d5ef9d21b7e64b
halulkin <halulkin@gmail.com>
2025-03-23 10:46:34 +0100

    feat: integrate Koin dependency injection for iOS and Android

--- composeApp/src/androidMain/kotlin/org/halulkin/MyApplication.kt
+++ composeApp/src/androidMain/kotlin/org/halulkin/MyApplication.kt
@@ -1,19 +1,16 @@
 package org.halulkin
 
 import android.app.Application
-import org.halulkin.feature.di.commonModule
-import org.halulkin.feature.di.platformModule
+import org.halulkin.feature.di.initKoin
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        startKoin {
-            androidContext(androidContext = this@MyApplication)
-            modules(platformModule(), commonModule())
+        initKoin {
+            androidContext(this@MyApplication)
         }
     }
 }
