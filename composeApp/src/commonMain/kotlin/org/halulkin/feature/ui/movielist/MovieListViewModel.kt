40d3b5321fe01395a56d6812b58fc78cbde7d65d
halulkin <halulkin@gmail.com>
2025-03-25 17:24:55 +0100

    feat: refactor MovieList to use Flow and Paging for movie data

--- composeApp/src/commonMain/kotlin/org/halulkin/feature/ui/movielist/MovieListViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/halulkin/feature/ui/movielist/MovieListViewModel.kt
@@ -2,12 +2,18 @@ package org.halulkin.feature.ui.movielist
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.cash.paging.cachedIn
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import org.halulkin.feature.domain.model.MovieType
+import org.halulkin.feature.domain.usecase.GetMoviesUseCase
 
 class MovieListViewModel(
+    private val getMoviesUseCase: GetMoviesUseCase,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
@@ -15,14 +21,27 @@ class MovieListViewModel(
         const val MOVIES_TYPE_KEY = "moviesType"
     }
 
-    private val moviesType: MovieType? = savedStateHandle.get<String>(MOVIES_TYPE_KEY)?.let {
-        MovieType.valueOf(it)
-    }
+    private val moviesType: MovieType = checkNotNull(
+        savedStateHandle.get<String>(MOVIES_TYPE_KEY)?.let {
+            MovieType.valueOf(it)
+        }
+    )
 
     private val _stateFlow: MutableStateFlow<MovieListState> = MutableStateFlow(MovieListState())
     val stateFlow: StateFlow<MovieListState> = _stateFlow.asStateFlow()
 
     init {
-        println("MovieListViewModel init: $moviesType")
+        loadMovies(moviesType)
+    }
+
+    private fun loadMovies(movieType: MovieType) = viewModelScope.launch {
+        getMoviesUseCase(movieType)
+            .onSuccess { movies ->
+                _stateFlow.update {
+                    it.copy(movies = movies.cachedIn(viewModelScope))
+                }
+            }.onFailure { error ->
+                println("Error: $error")
+            }
     }
 }
